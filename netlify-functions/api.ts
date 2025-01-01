8e0d3ca17266b77c87767d1d49220b1f31a343e5
Xio <mxbadano@gmail.com>
2025-01-01 15:27:57 -0300

    ft: netlify config

---
+++ netlify-functions/api.ts
@@ -0,0 +1,40 @@
+import express, { Express } from "express";
+import serverless from "serverless-http";
+import { rateLimiter } from "../src/utils/rateLimiter";
+import CompressionMiddleware from "../src/api/middlewares/compression.middleware";
+import { swaggerSetup } from "../docs/swagger";
+import router from "../src/api/routes/index.router";
+import { errorHandler } from "../src/api/middlewares/errorHandler";
+import mongoose from "mongoose";
+import { logger } from "../src/utils/logger";
+import config from "../src/config";
+
+const api: Express = express();
+
+// Middleware to parse JSON request body
+api.use(express.json());
+
+// Rate limit
+api.use(rateLimiter);
+
+// Compression middleware
+api.use(CompressionMiddleware);
+
+// Swagger setup
+api.use("/api-docs", swaggerSetup.serve, swaggerSetup.setup);
+
+api.use(router);
+
+// Error handler middleware
+api.use(errorHandler);
+
+mongoose
+	.connect(config.MONGO_URI)
+	.then(() => {
+		logger.info("MongoDB connected successfully");
+	})
+	.catch((err) => {
+		logger.error("Error connecting to MongoDB:", err);
+	});
+
+export const handler = serverless(api);
